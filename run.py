4d47c11e8f6ce65d38f8ae3ec3a3997252582c60
Michael Gachunji <mikegachunji@gmail.com>
2018-07-23 00:15:02 +0300

    Added use of instance folder

--- run.py
+++ run.py
@@ -1,9 +1,5 @@
 # run.py
  
-# Add your settings here... this is a temporary location, as the settings for a Flask app
-# should be stored separate from your main program.
-DEBUG = True
- 
 from ffr_env import app
  
 if __name__ == "__main__":
