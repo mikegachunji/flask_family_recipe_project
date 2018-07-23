f7c851a7101a4f08e552531ecc516a4cbcecd194
Michael Gachunji <mikegachunji@gmail.com>
2018-07-23 16:26:48 +0300

    Added use of templates and Bootstrap

--- ffr_env/tests/test_recipes.py
+++ ffr_env/tests/test_recipes.py
@@ -31,13 +31,11 @@ class ProjectTests(unittest.TestCase):
  
     def test_main_page(self):
         response = self.app.get('/', follow_redirects=True)
-        self.assertIn(b'Welcome to the Gachunji Family Recipe App!', response.data)
-        self.assertIn(b'This site describes our favorite family recipes!', response.data)
+        self.assertIn(b'Gachunji Family Recipes', response.data)
         self.assertIn(b'Breakfast Recipes', response.data)
         self.assertIn(b'Lunch Recipes', response.data)
         self.assertIn(b'Dinner Recipes', response.data)
         self.assertIn(b'Dessert Recipes', response.data)
- 
  
 if __name__ == "__main__":
     unittest.main()
