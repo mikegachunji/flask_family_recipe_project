70dc26bb147eb77a77394dcac9dac4fcb28852d2
Michael Gachunji <mikegachunji@gmail.com>
2018-07-26 18:28:18 +0300

    Added password hashing

--- ffr_env/__init__.py
+++ ffr_env/__init__.py
@@ -5,6 +5,7 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
  
 ################
 #### config ####
@@ -14,6 +15,7 @@ app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('flask.cfg')
  
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
